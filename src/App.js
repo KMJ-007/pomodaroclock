0b2ba8646d71d00c78b5377b6593c973306195d3
Karan Janthe <karanjanthe@gmail.com>
2021-12-21 17:04:31 +0530

    updated readme

--- src/App.js
+++ src/App.js
@@ -2,29 +2,30 @@ import React, { Component } from "react";
 import "./App.css";
 import Length from "./Length";
 
+const sound = document.getElementById("beep");
 
-const playBreaksound=()=>{
-  let  breakAudio= new Audio("./beep.mp3")
-  // this.breakAudio.setStatecurrentTime=0;
+const playBreaksound = () => {
+  let breakAudio = new Audio("./beep.mp3");
+  breakAudio.currentTime = 0;
   breakAudio.play();
-}
+};
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       display: 25 * 60,
-      breakTime: 5 * 60 ,
-      sessionTime:25*60,
-      timerOn:false,
-      onBreak:false,
+      breakTime: 5 * 60,
+      sessionTime: 25 * 60,
+      timerOn: false,
+      onBreak: false,
     };
     this.formateTime = this.formateTime.bind(this);
-    this.changeTime=this.changeTime.bind(this);
-    this.controlTime=this.controlTime.bind(this);
-    this.resetTime=this.resetTime.bind(this);
+    this.changeTime = this.changeTime.bind(this);
+    this.controlTime = this.controlTime.bind(this);
+    this.resetTime = this.resetTime.bind(this);
     // this.playBreaksound=this.playBreaksound.bind(this);
   }
-  
+
   formateTime = (time) => {
     let miniute = Math.floor(time / 60);
     let seconds = time % 60;
@@ -35,121 +36,131 @@ export default class App extends Component {
     );
   };
 
-  changeTime=(amount,type)=>{
-      if(type ==="break"){
-        if(this.state.breakTime<=60 && amount<0){
-          return;
-        }
-        this.setState({
-          breakTime:this.state.breakTime+amount
-        })
-      }else{
-        if(this.state.sessionTime<=60 && amount<0){
-          return;
-        }
-        this.setState({
-          sessionTime:this.state.sessionTime+amount
-
-        })
-        if(!this.state.timerOn){
-          this.setState({
-            display:this.state.display+amount
-          })
+  changeTime = (amount, type) => {
+    if (type === "break") {
+      if (this.state.breakTime <= 60 && amount < 0) {
+        return;
       }
-
+      this.setState({
+        breakTime: this.state.breakTime + amount,
+      });
+    } else {
+      if (this.state.sessionTime <= 60 && amount < 0) {
+        return;
       }
-  }//end of change time function
-  controlTime=()=>{
-    let s=1000;
+      this.setState({
+        sessionTime: this.state.sessionTime + amount,
+      });
+      if (!this.state.timerOn) {
+        this.setState({
+          display: this.state.display + amount,
+        });
+      }
+    }
+  }; //end of change time function
+  controlTime = () => {
+    let s = 1000;
     let date = new Date().getTime();
     let nextDate = new Date().getTime() + s;
-    let onBreakVariable=this.state.onBreak;
-    if(!this.state.timerOn){
-      let interval=setInterval(() => {
-        date=new Date().getTime();
-        if(date>nextDate){
-          if(this.state.display<=0 && !onBreakVariable ){
-            {playBreaksound()};
+    let onBreakVariable = this.state.onBreak;
+    if (!this.state.timerOn) {
+      let interval = setInterval(() => {
+        date = new Date().getTime();
+        if (date > nextDate) {
+          if (this.state.display <= 0 && !onBreakVariable) {
+            {
+              playBreaksound();
+            }
             this.setState({
-              onBreak:true
-            })
+              onBreak: true,
+            });
             this.setState({
-              display:this.state.breakTime
-            })
+              display: this.state.breakTime,
+            });
             // return this.state.breakTime;
-          }
-          else if(this.state.display<=0 && onBreakVariable ){
-            {playBreaksound()};
+          } else if (this.state.display <= 0 && onBreakVariable) {
+            {
+              playBreaksound();
+            }
             this.setState({
-              onBreak:false
-            })
-            
+              onBreak: false,
+            });
+
             this.setState({
-              display:this.state.sessionTime
-            })
+              display: this.state.sessionTime,
+            });
             // return this.state.sessionTime;
           }
           this.setState({
-            
-            display:this.state.display-1
-          })  
-          nextDate+=s;
+            display: this.state.display - 1,
+          });
+          nextDate += s;
         }
       }, 30);
-      
+
       // this.setState({
       //   timerOn:true
       // })
       localStorage.clear();
-      localStorage.setItem('interval-id',interval)
-    }//end of if
-    if(this.state.timerOn){
-      clearInterval(localStorage.getItem('interval-id'));
+      localStorage.setItem("interval-id", interval);
+    } //end of if
+    if (this.state.timerOn) {
+      clearInterval(localStorage.getItem("interval-id"));
     }
 
-      this.setState({
-        timerOn:!this.state.timerOn
-      })
-    
-  }//end of control time
-  resetTime=()=>{
+    this.setState({
+      timerOn: !this.state.timerOn,
+    });
+  }; //end of control time
+  resetTime = () => {
     this.setState({
       display: 25 * 60,
       breakTime: 5 * 60,
-      sessionTime:25*60,
-
-    })
-
-  }//end of resetTIme
+      sessionTime: 25 * 60,
+    });
+  }; //end of resetTIme
   render() {
     return (
-      <div>
+      <div id="container">
         <h1 className="main-title">Pomodaro Clock</h1>
         <div className="dualContainer">
-
-        <Length
-          title={"break length"}
-          changeTime={this.changeTime}
-          type={"break"}
-          time={this.state.breakTime}
-          formateTime={this.formateTime}
+          <Length
+            id="“Break Length”"
+            title={"break length"}
+            changeTime={this.changeTime}
+            type={"break"}
+            time={this.state.breakTime}
+            formateTime={this.formateTime}
           />
-        <Length
-          title={"session length"}
-          changeTime={this.changeTime}
-          type={"session"}
-          time={this.state.sessionTime}
-          formateTime={this.formateTime}
-        />
-          </div>
-        <h3>{this.state.onBreak? "Break" : "session"}</h3>
-        <h1 className="display-title">{this.formateTime(this.state.display)}</h1>
-        <button className="display-title" onClick={this.controlTime}>
-        {this.state.timerOn?"pause":"start"}
-        </button>
-        <button className="display-title" onClick={this.resetTime}>
-          Reset
-        </button>
+          <Length
+            id="session-label"
+            title={"session length"}
+            changeTime={this.changeTime}
+            type={"session"}
+            time={this.state.sessionTime}
+            formateTime={this.formateTime}
+          />
+        </div>
+        <h3 id="timer-label">{this.state.onBreak ? "Break" : "session"}</h3>
+        <h1 id="time-left" className="display-title">
+          {this.formateTime(this.state.display)}
+        </h1>
+        <div className="button-center">
+          <button
+            id="start_stop"
+            className="display-button"
+            onClick={this.controlTime}
+          >
+            {this.state.timerOn ? "pause" : "start"}
+          </button>
+          <button
+            id="reset"
+            className="display-button"
+            onClick={this.resetTime}
+          >
+            Reset
+          </button>
+        </div>
       </div>
     );
   }
--- src/Length.js
+++ src/Length.js
@@ -1,27 +1,28 @@
-import React from 'react'
+import React from "react";
 
-
-export default function Length({title,changeTime,type,time,formateTime}) {
-    return (
-       <div>
-           <h3 className='title-length'>{title}</h3>
-           <div className='time-set'>
-                <button
-                onClick={()=>{changeTime(-60,type)}}
-                >
-                    <i></i>
-                    down
-                </button>
-                <h3>{formateTime(time)}</h3>
-                <button
-                onClick={()=>{changeTime(60,type)}}
-
-                >
-                    <i></i>
-                    up
-                </button>
-           </div>
-
-       </div>
-    )
+export default function Length({ title, changeTime, type, time, formateTime }) {
+  return (
+    <div className="length-control">
+      <h3 className="title-length">{title}</h3>
+      <div className="time-set">
+        <button
+          id="break-decrement"
+          onClick={() => {
+            changeTime(-60, type);
+          }}
+        >
+          <i></i>-
+        </button>
+        <h3 id="break-length">{formateTime(time)}</h3>
+        <button
+          id="break-increment"
+          onClick={() => {
+            changeTime(60, type);
+          }}
+        >
+          <i></i>+
+        </button>
+      </div>
+    </div>
+  );
 }
